a7878565c303976cfe9d83581e75ed249e1c5402
KerttuRungi <kerttu.rungi@gmail.com>
2025-03-12 15:05:55 +0200

    SQL 12/03/2025 Commit
    Uued asjad

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,357 @@ use TARge24
 
 --db kustutamine
 drop database TARge24
+-- 1tund 26.02.25
+--loome db
+crEaTE database TARge24
+
+--db valimine
+use TARge24
+
+-- db kustutamine
+drop database TARge24
+
+-- 2tund 05.03.2025
+
+--tabeli loomine
+create table Gender
+(
+Id int not null primary key,
+Gender nvarchar(10) not null
+)
+
+--andmete sisestamine
+insert into Gender (Id, Gender)
+values (2, 'Male')
+insert into Gender (Id, Gender)
+values (1, 'Female'),
+(3, 'Unknown')
+
+--vaatame tabeli andmeid
+select * from Gender
+
+create table Person
+(
+Id int not null primary key,
+Name nvarchar(30),
+Email nvarchar(30),
+GenderId int
+)
+
+--andmete sisestamine
+insert into Person (Id, Name, Email, GenderId)
+values (1, 'Superman', 's@s.com', 2),
+(2, 'Wonderwoman', 'w@w.com', 1),
+(3, 'Batman', 'b@b.com', 2),
+(4, 'Aquaman', 'a@a.com', 2),
+(5, 'Catwoman', 'c@c.com', 1),
+(6, 'Antman', 'ant"ant.com', 2),
+(8, NULL, NULL, 2)
+
+select * from Person
+
+--võõrvõtme ühenduse loomine kahe tabeli vahel
+alter table Person add constraint tblPerson_GenderId_FK
+foreign key (GenderId) references Gender(Id)
+
+--- kui sisestad uue rea andmeid ja ei ole sisestanud GenderId alla väärtust, siis
+--- see automaatselt sisestab sellele reale väärtuse 3 e nagu meil on unknown
+alter table Person
+add constraint DF_Person_GenderId
+default 3 for GenderId
+
+select * from Person
+
+insert into Person (Id, Name, Email)
+values (7, 'Spiderman', 'spider@s.com')
+
+--piirangu kustutamine
+alter table Person
+drop constraint DF_Person_GenderId
+
+--lisame veeru
+alter table Person
+add Age nvarchar(10)
+
+--lisame nr piirangu vanuse sisestamisel
+alter table Person
+add constraint CK_Person_Age check (Age > 0 and Age < 155)
+
+-- rea kustutamine
+delete from Person where Id = 8
+
+select * from Person
+
+--kuidas uuendada andmeid
+update Person
+set Age = 19
+where Id = 7
+
+select * from Person
+
+--lisame veeru juurde
+alter table Person
+add City nvarchar(50)
+
+-- k]ik, kes elavad Gothami linnas
+select * from Person where City = 'Gotham'
+-- kõik, kes ei ela Gothamis
+select * from Person where City != 'Gotham'
+select * from Person where City <> 'Gotham'
+
+-- näitab teatud vanusega olevaid inimesi
+select * from Person where Age = 100 or Age = 45 or Age = 19
+select * from Person where Age in (100, 45, 19)
+
+-- näitab teatud vanusevahemikus olevaid inimesi
+select * from Person where Age between 27 and 31
+
+-- wildcard e näitab kõik g-tähega linnad
+select * from Person where City like 'g%'
+select * from Person where City like '%g%'
+
+--näitab kõiki, kellel ei ole @-märki emailis
+select * from Person where Email not like '%@%'
+
+-- n'tiab kõiki, kellel on emailis ees ja peale @-märki ainult üks täht
+select * from Person where Email like '_@_.com'
+
+-- kõik, kellel nimes ei ole esimene täht W, A, S
+select * from Person where Name Like '[^WAS]%'
+select * from Person
+
+--kõik, kes elavad Gothamis ja New Yorkis
+select * from Person where City in ('Gotham','New York')
+
+--- kõik, kes elavad välja toodud linnades ja on nooremad 
+--  kui 30 a 
+select * from Person where (City = 'Gotham' or City = 'New York')
+and Age < 30
+
+-- kuvab tähestikulises järjekorras inimesi ja võtab aluseks nime
+select * from Person order by Name
+-- kuvab vastupidises järjestuses
+select * from Person order by Name desc
+
+-- võtab kolm esimest rida
+select top 3 * from person
+
+-- kolm esimest, aga tabeli järjestus on Age ja siis Name
+select top 3 Age, Name from Person
+
+-- 3tund 12.03.2025
+
+--näita esimsesed 50% tabelis
+select top 50 percent * from Person
+
+--järjestab vanuse järgi isikud
+-- see päring ei järjesta numbreid õigesti kuna Age on nvarchar
+select * from Person order by Age desc
+
+--castime Age int andmetüübiks ja siis järjestab õigesti
+select * from Person order by CAST(Age as int)
+
+--kõikide isikute koondvanus
+select sum(cast(Age as int)) as TotalAge from Person
+
+--k6ige noorem isik
+select min(cast(Age as int)) from Person
+--k6ige vanem isik
+select max(cast(Age as int)) from Person
+
+--n4eme konkreetsetes linnades olevate isikute koondvanust
+--enne oli Age nvarchar, aga enne päringut muudame selle int-iks
+select City, sum(Age) as TotalAge from Person group by City
+
+--nüüd muudame muutuja andmetüüpi koodiga
+alter table Person
+alter column Name nvarchar(25)
+
+---kuvab esimeses reas välja toodud järjestuses ja kuvab Age-i TotalAge-iks
+---järjestab City-s olevate nimede järgi ja siis GenderId järgi
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId order by City 
+
+---näitab mitu rida on selles tabelis
+select count(*) from Person
+select * from Person
+
+---näitab tulemust, et mitu inimest on GenderId väärtusega 2 konkreetses linnas
+-- veel arvutab vanuse kokku
+select City, GenderId, sum(Age) as TotalAge, count(Id)as [Total Person(s)]
+from Person
+where GenderId = '2'
+group by GenderId, City
+
+insert into Person values
+(10, 'Black Panther', 'b@b.com', 2, 34, 'New york')
+
+--- näitab ära inimeste koondvanuse, kelle vanus on vähemalt 29 a 
+-- kui palju neid igas linnas elab
+select GenderId, City, sum(Age) as TotalAge, count(Id)
+as [Total Person(s)]
+from Person
+group by GenderId, City having sum(Age) > 29
+
+--loome tabelid Employees ja Department
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+insert into Employees(ID, Name, Gender, Salary, DepartmentId)
+VALUES
+(1, 'Tom', 'Male', 4000, 1),
+(2, 'Pam', 'Female', 3000, 3),
+(3, 'John', 'Male', 3500, 1),
+(4, 'Sam', 'Male', 4500, 2),
+(5, 'Todd', 'Male', 2800, 2),
+(6, 'Ben', 'Male', 7000, 1),
+(7, 'Sara', 'Female', 4800, 3),
+(8, 'Valarie', 'Female', 5500, 1),
+(9, 'James', 'Male', 6500, NULL),
+(10, 'Russell', 'Male', 8800, NULL);
+
+select * from Employees
+
+insert into Department(ID, DepartmentName, Location, DepartmentHead)
+VALUES
+(1, 'IT', 'London', 'Rick'),
+(2, 'Payroll', 'Delhi', 'Ron'),
+(3, 'HR', 'New York', 'Christie'),
+(4, 'Other Department', 'Sydney', 'Cinderella')
+
+select * from Department
+
+-- left join
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+
+--arvutab kõikide palgad kokku
+select SUM(cast(Salary as int)) from Employees
+-- kõige väiksema palgasaaja palk
+select min(cast(Salary as int)) from Employees
+-- ühe kuu palgafond linnade lõikes
+select Location, sum(cast(Salary as int)) as TotalSalary
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+group by Location
+
+alter table Employees
+add City nvarchar(30)
+
+-- sooline eripära palkade osas
+select City, Gender, sum(cast(Salary as int)) as TotalSalary
+from Employees
+group by City, Gender
+
+-- sama nagu eelmine aga linnad on tähestikulises järjekorras
+select City, Gender, sum(cast(Salary as int)) as TotalSalary
+from Employees
+group by City, Gender
+order by City
+
+-- loeb ära mitu rida andmeid on tabelis
+-- * asemel võib panna veel nimetusi
+select count(DepartmentId) from Employees
+
+-- mitu töötajat on soo ja linna kaupa selles tabelis
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+group by Gender, City
+
+-- näitab kõik mehed linnade kaupa
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+where Gender = 'Male'
+group by Gender, City
+
+-- näitab kõik naised linnade kaupa 
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+group by Gender, City
+having Gender = 'Female'
+
+-- kelle palk on vähemalt üle 4000
+select * from Employees where Salary > 4000
+
+-- kõigil, kellel on palk üle 4000 ja arvutab need kokku ning näitab soo kaupa
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, count (Id) as [Total Employee]
+from Employees group by Gender, City
+having cast(Salary as int) > 4000
+
+-- loome tabeli, milles hakkame automaatselt nummerdama Id-d
+create table Test1
+(
+Id int identity(1,1),
+Value nvarchar(20)
+)
+insert into Test1 values('X')
+select * from Test1
+
+-- kustutame ära City veeru Employees tabelist
+alter table Employee
+drop colum City
+
+--- inner join
+-- kuvab neid, kellel on DepartmentName all olemas väärtus
+select Name, Gender, Salary, DepartmentName
+from Employees
+inner join Department
+on Employees.DepartmentId = Department.Id
+
+-- left join
+-- kuidas saada kõik andmed Employees tabelist kätte
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department -- võib kasutada ka LEFT OUTER JOIN
+on Employees.DepartmentId = Department.Id
+
+-- right join  
+-- kuidas saada Departmentname alla uus nimetus e antud juhul Other Department
+select Name, Gender, Salary, DepartmentName
+from Employees
+right join Department -- võib kasutada ka LEFT OUTER JOIN
+on Employees.DepartmentId = Department.Id
+
+-- full outer join
+select Name, Gender, Salary, DepartmentName
+from Employees
+full outer join Department
+on Employees.DepartmentId = Department.Id
+
+--- cross join võtab kaks allpool olevat tabelit kokku ja korrutab need omavahel läbi
+select Name, Gender, Salary, DepartmentName
+from Employees
+cross join Department
+
+-- päringu sisu ehk loogika
+select ColumnList
+from LeftTable
+joinType RightTable
+on joinCondition
+
+-- kuidas kuvada ainult need isikud kellel on DepartmentName NULL
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+where Employees.DepartmentId is null
+
